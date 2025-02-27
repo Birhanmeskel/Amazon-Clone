dce8b1d2ac860d9e5660151320572f25588a3cab
Birhan <birhan.coder7@gmail.com>
2025-02-27 18:52:47 +0300

    detail page routing

--- amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
+++ amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import LayOut from '../../Components/LayOut/LayOut'
+import React, { useEffect, useState } from "react";
+import LayOut from "../../Components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoints";
+import ProductCard from "../../Components/product/ProductCard"
 
 const ProductDetail = () => {
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(()=>{
+        setLoading(false)
+      });
+  }, [productId]);
+
+  if(loading) return <h3>Loading...</h3>
+   if (!product) return <p>product not found...</p>;
+
   return (
     <LayOut>
-      product Detail page
+      <ProductCard product={product} key={product.id} />
     </LayOut>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
--- amazon-clone/src/Routing.jsx
+++ amazon-clone/src/Routing.jsx
@@ -6,6 +6,7 @@ import Payment from './Pages/Payment/Payment'
 import Cart from './Pages/Cart/Cart'
 import Orders from './Pages/Orders/Orders'
 import Results from './Pages/Results/Results'
+import ProductDetail from './Pages/ProductDetail/ProductDetail'
 
 const Routing = () => {
   return (
@@ -16,6 +17,8 @@ const Routing = () => {
         <Route path="/payments" element={<Payment />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/products/:productId" element={<ProductDetail/> } />
+
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
